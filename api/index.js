4da1249bd6b05a4e6477ea4ade5e839fa2a70b42
adelsadek <adel.sadek@ihorizons.com>
2024-09-16 19:04:49 +0300

    - add port

--- api/index.js
+++ api/index.js
@@ -3,12 +3,11 @@ const cors = require("cors");
 const app = express();
 const connectDB = require("../config/db");
 const productRoutes = require("../routes/productRoutes");
-const serverless = require('serverless-http');
 
 
 
 const products = [
-  { id: 1, name: 'adel 1eee', price: 100 },
+  { id: 1, name: 'adwwwwel 1eee', price: 100 },
   { id: 2, name: 'Product 2www', price: 200 },
   { id: 3, name: 'Product 3eeee3333', price: 300 }
 ];
@@ -39,8 +38,8 @@ app.get('/products', (req, res) => {
 
 
 // // Start the server
-// app.listen(3000, () => {
-//   console.log(`Server is running on http://localhost:${3000}`);
-// });
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${3000}`);
+});
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
