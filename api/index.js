8cd1bfa7995ed647fbd7b69ff33d7e31422cb639
adelsadek <adel.sadek@ihorizons.com>
2024-09-16 18:31:00 +0300

    - update endpoints - data

--- api/index.js
+++ api/index.js
@@ -4,11 +4,24 @@ const app = express();
 const connectDB = require("../config/db");
 const productRoutes = require("../routes/productRoutes");
 
+
+
+const products = [
+  { id: 1, name: 'Product 1', price: 100 },
+  { id: 2, name: 'Product 2', price: 200 },
+  { id: 3, name: 'Product 3', price: 300 }
+];
+
 // Connect to database
 connectDB().then(() => {
   app.listen(3000, () => {
     console.log(`Server is running at 3000`);
     // Register routes
+
+    app.get('/test', (req, res) => {
+      res.json(products);
+    });
+
     app.use("/products", productRoutes);
   });
 });
