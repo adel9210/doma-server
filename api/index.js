3920b64a1403443d879dd501fa87e4af2efa88de
adelsadek <adel.sadek@ihorizons.com>
2024-09-16 19:52:38 +0300

    - add some fixes - fix cors

--- api/index.js
+++ api/index.js
@@ -5,26 +5,18 @@ const connectDB = require("../config/db");
 const productRoutes = require("../routes/productRoutes");
 
 
-
-const products = [
-  { id: 1, name: 'adwwwwel 1eee', price: 100 },
-  { id: 2, name: 'Product 2www', price: 200 },
-  { id: 3, name: 'Product 3eeee3333', price: 300 }
-];
-
-
-
 // Connect to database
 connectDB().then(() => {
   console.log('Connected Success!')
 }).catch(err =>{
   console.log('Error While Connecting DB')
 })
-app.use("/products", productRoutes);
 
 app.use(cors()); // Allow all origins by default
 
 app.use(express.json());
+
+app.use("/products", productRoutes);
 
 
 // // Start the server
