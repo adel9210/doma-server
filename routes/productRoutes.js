b1ff23ffeda2c441fe9f0de87223bb40339b8f02
adelsadek <adel.sadek@ihorizons.com>
2024-09-22 13:28:10 +0300

    - fix cors - updated - remove all 1

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 // Specific routes first
-// router.get('/:id', productController.getProductById); // Get product by ID
-router.delete('/delete-product/:id', productController.deleteProduct); // Delete product by ID
-// router.put('/:id', productController.updateProduct); // Update product by ID
+router.get('/:id', productController.getProductById); // Get product by ID
+router.delete('/:id', productController.deleteProduct); // Delete product by ID
+router.put('/:id', productController.updateProduct); // Update product by ID
 
 // General routes
-// router.get('/', productController.getAllProducts); // Get all products
-// router.post('/', productController.createProduct); // Create a new product
-// router.post('/upload-images', productController.uploadImages); // Upload images
-// router.delete('/delete-image/:filename', productController.deleteFile); // Delete image by filename
+router.get('/', productController.getAllProducts); // Get all products
+router.post('/', productController.createProduct); // Create a new product
+router.post('/upload-images', productController.uploadImages); // Upload images
+router.delete('/delete-image/:filename', productController.deleteFile); // Delete image by filename
 
 module.exports = router;
