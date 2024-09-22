a7b47085d19aaf8b50d3a36187c1eda43ca8d5c9
adelsadek <adel.sadek@ihorizons.com>
2024-09-22 12:02:33 +0300

    - fix cors - updated

--- api/index.js
+++ api/index.js
@@ -6,36 +6,16 @@ const productRoutes = require("../routes/productRoutes");
 const orderRoutes = require("../routes/orderRoutes");
 const path = require('path');
 
-const allowedOrigins = ['https://doma-store.vercel.app'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or Postman)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
 // Connect to database
 connectDB().then(() => {
   console.log('Connected Success!')
 }).catch(err =>{
   console.log('Error While Connecting DB')
-
 })
 
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-
-// app.options('*', cors()); // Enable preflight for all routes
-
-
+app.use(cors()); // Should be before defining routes
+// app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 app.use(express.json());
-
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
